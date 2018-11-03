2f635209d0df42215f065d656444d8f4df511c0e
ekarat2540 <ekarat.suk@hotmail.com>
2018-11-04 01:01:20 +0700

    first

---
+++ HW01_59160993.php
@@ -0,0 +1,171 @@
+<!doctype html>
+    <head>
+        <title>59160993</title>
+        <link rel="stylesheet" href="assets/css/animate.css">
+        <link rel="stylesheet" href="assets/css/iconfont.css">
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+        <link rel="stylesheet" href="assets/css/bootsnav.css">
+
+
+        <!--Theme custom css -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        <!--<link rel="stylesheet" href="assets/css/colors/maron.css">-->
+
+        <!--Theme Responsive css-->
+        <link rel="stylesheet" href="assets/css/responsive.css" />
+
+        <script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+
+    <body data-spy="scroll" data-target=".navbar-collapse">
+
+
+        <!-- Preloader -->
+        <div id="loading">
+            <div id="loading-center">
+                <div id="loading-center-absolute">
+                    <div class="object" id="object_one"></div>
+                    <div class="object" id="object_two"></div>
+                    <div class="object" id="object_three"></div>
+                    <div class="object" id="object_four"></div>
+                </div>
+            </div>
+        </div><!--End off Preloader -->
+
+
+        <div class="culmn">
+            <!--Home page style-->
+
+
+            <nav class="navbar navbar-default bootsnav navbar-fixed black no-background">
+                <div class="container">  
+
+                    <div class="attr-nav">
+                        <ul>
+                            <li class="side-menu"><a href="#"><i class="fa fa-bars"></i></a></li>
+                        </ul>
+                    </div>  
+
+
+                    <!-- Start Header Navigation -->
+                    <div class="navbar-header">
+
+                        <a class="navbar-brand" href="#brand">
+                            Ekarat
+                        </a>
+
+                    </div>
+                    <!-- End Header Navigation -->
+                </div>   
+
+
+                <!-- Start Side Menu -->
+                <div class="side">
+                    <a href="#" class="close-side"><i class="fa fa-times"></i></a>
+                    <div class="widget">
+                        <h6 class="title">59160993 Portfolio</h6>
+                        <ul class="link main-nav">
+                            <li><a href="#home">Home</a></li>
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#portfolio">Portfolio</a></li>
+                            <li><a href="#choose">Choose</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- End Side Menu -->
+            </nav>
+
+
+            
+
+
+            <!--About Section--> 
+            <section id="about" class="about bg-light roomy-150">
+                <div class="container">
+                    <div class="row">
+                        <div class="main_about">
+                            <div class="col-md-5 col-md-offset-1">
+                                <div class="about_content">
+                                    <div class="head_right">
+                                        <h2>About me</h2>
+                                    </div>
+                                    <div class="about_content_text">
+                                            <p>นายเอกราช สุขประเสริฐ นักศึกษามหาวิทยาลัยบูรพา <br />
+                                                คณะวิทยาการสารสนเทศ สาขาวิทยาการคอมพิวเตอร์ ชั้นปีที่ 3
+                                                <br />
+                                                <br />
+                                                email : 59160993@go.buu.ac.th<br />
+                                                tel : 0972311021<br />
+                                            </p>
+
+                                            <div class="divider1 m-top-10"></div>
+
+                                            <div class="about_content_item m-top-30">
+                                                <h6><i class="fa fa-graduation-cap"></i>ประวัติการศึกษา</h6>
+                                                <ul>
+                                                    <li><a href=""><i class="fa fa-chevron-right"></i>อนุบาล-ประถม</a></li>
+                                                    <p>จบจากโรงเรียนสุเหร่าคลองจั่น ตั้งแต่ อนุบาล 1 ถึง ประถมศึกษา 6
+                                                    </p>
+                                                </ul>
+    
+                                                <ul>
+                                                        <li><a href=""><i class="fa fa-chevron-right"></i>มัธยม</a></li>
+                                                        <p>จบจากโรงเรียนบางกะปิ ตั้งแต่ มัธยมศึกษา 1 ถึง มัธยมศึกษา 6
+                    
+                                                        </p>
+                                                    </ul>
+                                            </div>
+                                            <div class="about_content_item m-top-20">
+                                                <h6><i class="fa fa-briefcase"></i>การศึกษาหรือการทำงานปัจจุบัน</h6>
+                                                <ul>
+                                                    <li><i class="fa fa-chevron-right"></i>ปัจจุบันศึกษาอยู่ที่มหาวิทยาลัยบูรพา</li>
+                                                    <p>ศึกษาคณะวิทยาการสารสนเทศ สาขาวิทยาการคอมพิวเตอร์</p>
+                                                </ul>
+                                            </div>
+                                    </div>
+                                </div>
+                            </div><!-- End off col-md-6 -->
+
+                            <div class="col-md-5 col-md-offset-1 sm-m-top-50">
+                                <div class="head_title text-right">
+                                    <h5><span class="divider"></span>My name is Ekarat Sukprasert 59160993</h5>
+                                </div>
+                                <div class="about_left_item m-top-50">
+                                    <img src="assets/images/a.jpg" alt="" />
+                                </div>
+
+                                <div class="about_socail m-top-50">
+                                    <ul class="list-inline">
+                                        <li><span class="divider"></span></li>
+                                        <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href=""><i class="fa fa-dribbble"></i></a></li>
+                                        <li><a href=""><i class="fa fa-behance"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div><!-- End off col-md-6 -->
+
+                        </div><!-- End off Main About -->
+
+                    </div><!-- End off row -->
+                </div><!-- End off container -->
+            </section><!-- End off About us Section -->
+            <!-- JS includes -->
+
+            <script src="assets/js/vendor/jquery-1.11.2.min.js"></script>
+            <script src="assets/js/vendor/bootstrap.min.js"></script>
+
+            <script src="assets/js/jquery.magnific-popup.js"></script>
+            <script src="assets/js/jquery.easing.1.3.js"></script>
+            <script src="assets/js/bootsnav.js"></script>
+
+
+
+            <script src="assets/js/plugins.js"></script>
+            <script src="assets/js/main.js"></script>
+
+    </body>
+</html>
